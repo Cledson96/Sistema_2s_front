b1ecf3ff9f587ac4a81e66e102c1171fd589ef56
Cledson <cledson1996@gmail.com>
2022-12-04 23:22:01 -0300

    dormir

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -12,6 +12,14 @@ export default function Inicio({ setdados }) {
             <div className='fundo_inicio'>
                 <div className='menu_lateral'>
                     <h1 className='logado'> Logado: {nome}</h1>
+                    <ul>
+                        <li> Cadastrar usuario</li>
+                        <li> Cadastrar motoboy</li>
+                        <li> Entrada pedidos </li>
+                        <li> Informações pedidos </li>
+                        <li>Informações motoboys</li>
+                        <li className='sair'> sair</li>
+                    </ul>
                 </div>
             </div>
         </div>
