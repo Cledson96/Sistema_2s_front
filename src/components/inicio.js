13a336a2e2a7bf503d21452dfab9e2ef3da75c61
Cledson <cledson1996@gmail.com>
2022-12-06 00:35:48 -0300

    dormir

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -1,10 +1,24 @@
 import './style.css'
 import logo from '../img/motoboy-curitiba-logotipo.png'
 import { Link } from 'react-router-dom'
+import { getmotoboys } from './requisicao'
+import { useEffect, useState } from 'react';
 
 
 export default function Inicio({ setdados }) {
+    const [carregando, setcarregando] = useState([]);
+    const [boys, setboys] = useState([])
     let nome = localStorage.getItem("nome_logado");
+
+    useEffect(() => {
+        let resposta = getmotoboys()
+        resposta.then((res) => {
+            setboys(res.data)
+
+        });
+        resposta.catch(() => alert("Tivemos um problema para atualizar os motoboys!!"))
+    }, carregando);
+
     return (
         <div className="sistema">
             <div className="header">
@@ -14,13 +28,22 @@ export default function Inicio({ setdados }) {
                 <div className='menu_lateral'>
                     <h1 className='logado'> Logado: {nome}</h1>
                     <ul>
-                        <Link to={'/cadastro'}><li> Cadastrar usuario</li></Link>
-                        <li> Cadastrar motoboy</li>
+                        <Link className="link" to={'/inicio'}> <li> Tela inicial</li></Link>
+                        <Link className="link" to={'/cadastro'}><li> Cadastrar usuario</li></Link>
+                        <Link className="link" to={'/cadastro_motoboy'}> <li> Cadastrar motoboy</li></Link>
                         <li> Entrada pedidos </li>
                         <li> Informações pedidos </li>
                         <li>Informações motoboys</li>
                         <li className='sair'> sair</li>
                     </ul>
+                </div>
+                <div className="inicio">
+                    <Link className="link" to={'/motoboys'}>
+                        <div className="motoboys">
+                            <h1 className="card">Motoboys cadastrados:</h1>
+                            <h2 className="qtd" >{boys.length ? boys.length : "0"}</h2>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
